65df173b3a56791c80031a7734a46a29eef84e6c
Aashish909 <aashish24062000@gmail.com>
2026-06-05 18:27:33 +0530

    fix: update service URLs to production endpoints in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,12 +8,18 @@ import "leaflet/dist/leaflet.css";
 import { SocketProvider } from "./context/SocketContext.tsx";
 
 
-export const authService = "http://localhost:5000";
-export const storeService = "http://localhost:5001";
-export const utilsService = "http://localhost:5002";
-export const realtimeService = "http://localhost:5004";
-export const riderService = "http://localhost:5005";
-export const adminService = "http://localhost:5006";
+// export const authService = "http://localhost:5000";
+export const authService = "https://gokart-auth.onrender.com";
+// export const storeService = "http://localhost:5001";
+export const storeService = "https://gokart-store.onrender.com";
+// export const utilsService = "http://localhost:5002";
+export const utilsService = "https://gokart-utils.onrender.com";
+// export const realtimeService = "http://localhost:5004";
+export const realtimeService = "https://gokart-realtime.onrender.com";
+// export const riderService = "http://localhost:5005";
+export const riderService = "https://gokart-rider.onrender.com";
+// export const adminService = "http://localhost:5006";
+export const adminService = "https://gokart-admin.onrender.com";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
